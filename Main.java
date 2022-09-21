5d9cedf7a84da8cf0573de52757a0bf61b61a764
SnehaPoonacha <snehapoonacaha12@gmail.com>
2022-09-21 14:17:30 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+package com.example.myapplication10;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+TextView marqtxt;
+Button btnstart, btnstop;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+marqtxt = (TextView) findViewById(R.id.marqueeText);
+btnstart = (Button) findViewById(R.id.buttonstart);
+btnstop = (Button) findViewById(R.id.buttonstop);
+btnstart.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+ExampleAsyncTask exampleAsncTask=new ExampleAsyncTask();
+exampleAsncTask.execute();
+}
+});
+btnstop.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+    marqtxt.setSelected(false);
+    marqtxt.setVisibility(View.INVISIBLE);
+    }
+    });
+    }
+    private class ExampleAsyncTask extends AsyncTask<String, String,String> {
+    @Override
+    protected void onPreExecute() {
+    super.onPreExecute();
+    Toast.makeText(getBaseContext(),"Async Task Started!!!!!!!!",Toast.LENGTH_SHORT).show();
+    } @Override
+    protected String doInBackground(String... strings) {
+    try
+    {
+    Thread.sleep(250);
+    }
+    catch (InterruptedException e)
+    {
+    e.printStackTrace();
+    } return null;
+    } @Override
+    protected void onPostExecute(String s) {
+    super.onPostExecute(s);
+    marqtxt.setVisibility(View.VISIBLE);
+    marqtxt.setSelected(true);
+    }
+    }
